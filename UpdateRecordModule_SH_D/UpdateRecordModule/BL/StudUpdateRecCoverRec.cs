f291b3a0869f6774fd4ddf59fbb5728dc90b238e
ED <yinghua.lin@ischool.com.tw>
2018-02-05 18:04:25 +0800

    2018/2/5 穎驊完成封面名冊儲存功能， 使用者可以使用介面輸入封面名冊的資料 修改後上傳。

---
+++ UpdateRecordModule_SH_D/UpdateRecordModule/BL/StudUpdateRecCoverRec.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace UpdateRecordModule_SH_D.BL
+{
+    //2018/2/5 穎驊 新增異動名冊封面物件
+
+    /// <summary>
+    /// 異動名冊類別(使用在存取異動名冊)
+    /// </summary>
+    public class StudUpdateRecCoverRec
+    {
+        /// <summary>
+        /// 年級
+        /// </summary>
+        public string grYear { get; set; }
+
+        /// <summary>
+        /// 科別代碼
+        /// </summary>
+        public string DeptCode { get; set; }
+
+
+        /// <summary>
+        /// 名冊別
+        /// </summary>
+        public string ReportType { get; set; }
+
+        /// <summary>
+        /// 班別
+        /// </summary>
+        public string ClassType { get; set; }
+        /// <summary>
+        /// 上傳類別
+        /// </summary>
+        public string UpdateType { get; set; }
+        /// <summary>
+        /// 核定班數
+        /// </summary>
+        public string ApprovedClassCount { get; set; }
+        /// <summary>
+        /// 核定學生數
+        /// </summary>
+        public string ApprovedStudentCount { get; set; }
+        /// <summary>
+        /// 實招班數
+        /// </summary>
+        public string ActualClassCount { get; set; }
+        /// <summary>
+        /// 實招新生數
+        /// </summary>
+        public string ActualStudentCount { get; set; }
+
+        /// <summary>
+        /// 原有學生數
+        /// </summary>
+        public string OriginalStudentCount { get; set; }
+
+        /// <summary>
+        /// 增加學生數
+        /// </summary>
+        public string IncreaseStudentCount { get; set; }
+
+        /// <summary>
+        /// 減少學生數
+        /// </summary>
+        public string DecreaseStudentCount { get; set; }
+
+        /// <summary>
+        /// 更正學生數
+        /// </summary>
+        public string ModifiedStudentCount { get; set; }
+
+        /// <summary>
+        /// 現有學生數
+        /// </summary>
+        public string CurrentStudentCount { get; set; }
+
+        /// <summary>
+        /// 註1
+        /// </summary>
+        public string Remarks1 { get; set; }
+
+        /// <summary>
+        /// 備註說明
+        /// </summary>
+        public string RemarksContent { get; set; }
+
+    }
+}
--- UpdateRecordModule_SH_D/UpdateRecordModule/GovernmentalDocument/ModifyingCoverForm.Designer.cs
+++ UpdateRecordModule_SH_D/UpdateRecordModule/GovernmentalDocument/ModifyingCoverForm.Designer.cs
@@ -28,34 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridViewX1 = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            this.btnSave = new DevComponents.DotNetBar.ButtonX();
+            this.btnClose = new DevComponents.DotNetBar.ButtonX();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewX1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewX1
             // 
+            this.dataGridViewX1.AllowUserToAddRows = false;
+            this.dataGridViewX1.AllowUserToDeleteRows = false;
+            this.dataGridViewX1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridViewX1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridViewX1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewX1.DefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridViewX1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(215)))), ((int)(((byte)(229)))));
             this.dataGridViewX1.Location = new System.Drawing.Point(12, 69);
             this.dataGridViewX1.Name = "dataGridViewX1";
             this.dataGridViewX1.RowTemplate.Height = 24;
-            this.dataGridViewX1.Size = new System.Drawing.Size(1123, 335);
+            this.dataGridViewX1.Size = new System.Drawing.Size(1123, 298);
             this.dataGridViewX1.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnSave.BackColor = System.Drawing.Color.Transparent;
+            this.btnSave.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnSave.Location = new System.Drawing.Point(939, 397);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(99, 23);
+            this.btnSave.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "儲存調整結果";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnClose.Location = new System.Drawing.Point(1060, 397);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "取消";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // ModifyingCoverForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1147, 432);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.dataGridViewX1);
             this.DoubleBuffered = true;
             this.Name = "ModifyingCoverForm";
@@ -68,5 +103,7 @@
         #endregion
 
         private DevComponents.DotNetBar.Controls.DataGridViewX dataGridViewX1;
+        private DevComponents.DotNetBar.ButtonX btnSave;
+        private DevComponents.DotNetBar.ButtonX btnClose;
     }
 }
